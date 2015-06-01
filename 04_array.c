fdb52facd7d7e00db8efd637dd09ac3e8798e455
Trent Robbins <robbintt@gmail.com>
2015-05-31 20:54:56 -0700

    Added array and array pointer.

---
+++ 04_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+ * There are some interesting questions here. 
+ *
+ * Why is the memory address in the for loop unchanged?
+ *
+ *
+ *
+ */
+
+int main() {
+    int i;
+    int array[5];
+    int *array_ptr = array;
+    printf("Value of i: %d\n", array[0]);
+    printf("Memory Address: %p\n", array_ptr);
+    /*
+    for(i=0; i<10; i++) {
+        int result = add_one(i);
+        int *result_pointer = &result;
+        printf("Value of i: %d\n", result);
+        printf("Memory Address: %p\n", result_pointer);
+    }
+    */
+    return 0;
+}
+
+int add_one(int input) {
+    // Must have the ++ before the variable name or else it adds after setting
+    // result to input.
+    int result = input+1;    
+    return result;
+}
