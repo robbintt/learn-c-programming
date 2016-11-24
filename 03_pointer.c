d29b1e94dd8e88ccca63233614c08df42221fea7
Trent Robbins <robbintt@gmail.com>
2016-11-24 12:42:44 -0800

    Fixed: called a function before it was declared (c99)

--- 03_pointer.c
+++ 03_pointer.c
@@ -5,6 +5,13 @@
  * / <--(note the space between * / means this line is still a comment.
  * This sequence ends a multiline comment -> */  
 
+int add_one(int input) {
+  // Must have the ++ before the variable name or else it adds after setting
+  // result to input.
+  int result = input+1;    
+  return result;
+}
+
 int main() {
   int i;
   for(i=0; i<10; i++) {
@@ -16,9 +23,3 @@ int main() {
   return 0;
 }
 
-int add_one(int input) {
-  // Must have the ++ before the variable name or else it adds after setting
-  // result to input.
-  int result = input+1;    
-  return result;
-}
