f42fe42cd0455f03e1b401c99f0061a6373e7743
Trent Robbins <robbintt@gmail.com>
2016-11-24 16:03:30 -0800

    Fixed 04.

--- 04_array.c
+++ 04_array.c
@@ -15,18 +15,19 @@ int add_one(int input) {
 }
 
 int main() {
+
   int len = 10;
-  
-  // choose one of the two following declarations
-  // int *(array+len)
   int array[len];
 
   for(int i=0; i<len; i++) {
     int result = add_one(i);
-    array[i] = result;
+    // two options for how to access array:
+    // array[i] = result;
+    *(array + i) = result;
     printf("Value of i: %d\n", array[i]);
     printf("Memory Address: %p\n", &array[i]);
   }
+
   return 0;
 }
 
