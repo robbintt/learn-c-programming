39fdbdfeca48f3b412d6c755931f8d8ccc0999de
Trent Robbins <robbintt@gmail.com>
2016-11-25 19:11:45 -0800

    Did extra credit.

--- lcthw/ex7.c
+++ lcthw/ex7.c
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
   printf("You have %d bugs at the imaginary rate of %f.\n", 
           bugs, bug_rate);
 
-  long universe_of_defects = 1L * 1024L * 1024L * 1024L;
-  printf("The entire universe has %ld bugs.\n", universe_of_defects);
+  unsigned long universe_of_defects = 1L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L;
+  printf("The entire universe has %lu bugs.\n", universe_of_defects);
 
   double expected_bugs = bugs * bug_rate;
   printf("You are expected to have %f bugs.\n", expected_bugs);
