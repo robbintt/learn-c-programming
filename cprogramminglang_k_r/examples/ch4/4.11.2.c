268f3466d8a01a00659026a73543ad8b47c41494
Trent Robbins <robbintt@gmail.com>
2017-02-20 12:53:21 -0800

    Added much more detail to different macro edge cases.

--- cprogramminglang_k_r/examples/ch4/4.11.2.c
+++ cprogramminglang_k_r/examples/ch4/4.11.2.c
@@ -10,12 +10,29 @@
 
 #define forever for (;;) { printf("c"); \
                            printf("0");}; /* infinite loop */
+// example of undef (uncomment result)
+#undef forever
 
 #define max(A, B) ((A) > (B) ? (A) : (B))
+
+// the macro is undef'd above this declaration
+int forever()
+{
+  printf("not forever\n");
+  return 0;
+}
 
 int main()
 {
   int p = 1, q = 2, r = 3, s = 4;
-  printf("%i\n", max(p+q, r+s));
-  // forever  // note the semicolon is in the macro, too.
+  // max() result is 7 = (r+s)
+  printf("%i\n", max(p+q, r+s)); // note the semicolon is in the macro.
+
+  // note that a macro cannot have a semicolon if it isn't the end of its substitution.
+  // putting a semicolon at the end of the macro is bad for composability but it does work when syntactically correct.
+  // forever // the macro has no () and contains the ; for fun (because it ends the line)
+  forever();
 }
+
+
+
