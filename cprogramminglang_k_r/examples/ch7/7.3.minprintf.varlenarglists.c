ffc7ee81d05575ab5a87d92006ce351a89dfb57f
Trent Robbins <robbintt@gmail.com>
2017-05-14 12:46:13 -0700

    Added minprintf with variable # of args example.

---
+++ cprogramminglang_k_r/examples/ch7/7.3.minprintf.varlenarglists.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+
+// not included in k&r p.156
+#include <stdio.h>
+
+/* minprintf: minimal printf with variable argument list */
+/* printf returns the character count and is declared as int */
+void minprintf(char *fmt, ...)
+{
+  va_list ap; /* points to each unnamed arg in turn */
+  char *p, *sval;
+  int ival;
+  double dval;
+
+  va_start(ap, fmt); /* make ap point to 1st unnamed arg in turn */
+  for (p = fmt; *p; p++) {
+    if (*p != '%') {
+      putchar(*p);
+      continue;
+    }
+    switch (*++p) {
+    case 'd':
+      ival = va_arg(ap, int);
+      printf("%d", ival);
+      break;
+    case 'f':
+      dval = va_arg(ap, double);
+      printf("%f", dval);
+      break;
+    case 's':
+      for (sval = va_arg(ap, char *); *sval; sval++)
+        putchar(*sval);
+      break;
+    default:
+      putchar(*p);
+      break;
+    }
+  }
+  va_end(ap); /* clean up when done */
+}
+
+int main()
+{
+  minprintf("hello world!\n");
+  // must include a \0 at the end of any string OR
+  // give the number of characters e.g. %1s
+
+  // be sure to use double quotes on the string, single quotes denote char
+  minprintf("int: %d double: %f, str: %s\n", 5, 5.555555, "hello world!");
+  return 0;
+}
